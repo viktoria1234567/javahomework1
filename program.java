cb49b537ad64cd887e6dbe7a542dda3c12bd6750
Viktoria <viktoriapredko1@gmail.com>
2022-11-15 16:33:06 +0300

    Solve task4

--- program.java
+++ program.java
@@ -35,22 +35,43 @@ import java.util.Scanner;
 //2.Написать метод, принимающий на вход два целых числа и проверяющий, что их сумма лежит в пределах от 10 до 20 (включительно),
 // если да – вернуть true, в противном случае – false.
 
-    // public class program{
-    //     public static void main(String[] args) {
-    //         Scanner iScanner = new Scanner(System.in);
-    //         Integer n = iScanner.nextInt();
-    //         Integer m = iScanner.nextInt();
+// public class program{
+//     public static void main(String[] args) {
+//         Scanner iScanner = new Scanner(System.in);
+//         Integer n = iScanner.nextInt();
+//         Integer m = iScanner.nextInt();
 
-    //         method(n,m);  
-    //     }  
+//         method(n,m);  
+//     }  
         
-    //     private static void method(int x, int y) {
-    //         int sum = x+y;
-    //         if (sum<=20 && sum >= 10){
-    //             System.out.println(true);
-    //         }
-    //         else{
-    //             System.out.println(false);
-    //         }   
-    //      }   
-    // }    
+//     private static void method(int x, int y) {
+//         int sum = x+y;
+//         if (sum<=20 && sum >= 10){
+//             System.out.println(true);
+//         }
+//         else{
+//             System.out.println(false);
+//         }   
+//     }   
+// }    
+// 3.Написать метод, которому в качестве параметра передается целое число, метод должен напечатать в консоль, 
+// положительное ли число передали или отрицательное. Замечание: ноль считаем положительным числом.
+
+// public class program{
+//     public static void main(String[] args) {
+//         Scanner iScanner = new Scanner(System.in);
+//         Integer n = iScanner.nextInt();
+            
+
+//         method(n);  
+//     } 
+//     private static void method(int x) {
+//         if (x>=0){
+//             System.out.println("число положительное"); 
+//          }   
+//         else{
+//              System.out.println("число отрицательное");
+//         }                     
+//     }      
+// }
+
