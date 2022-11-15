134e581e4cd2d346cd68cfe717fd3c60c2ea9dce
Viktoria <viktoriapredko1@gmail.com>
2022-11-15 16:21:16 +0300

    Solve task2

---
+++ program.java
@@ -0,0 +1,56 @@
+//1.Вычислить n-ое треугольного число(сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+
+import java.util.Scanner;
+//     public class program{
+//         public static void main(String[] args) {
+//             Scanner iScanner = new Scanner(System.in);
+//             //System.out.printf("n: ");
+//             Integer n = iScanner.nextInt();
+//             // System.out.printf("Привет. %s!", name);
+//             // iScanner.close();
+//             method(n);
+//             method2(n); 
+//         }   
+        
+//         private static void method(int x) {
+//             int i = 1;
+//             int sum = 0;
+//             while (i <= x){
+//                 sum+=i;
+//                 i+=1;   
+//             }
+//             System.out.println(sum);
+//         }   
+//         private static void method2(int y) {
+//             int i = 1;
+//             int fact = 1;
+//             while (i <= y){
+//                 fact*=i;
+//                 i+=1;   
+//             }
+//             System.out.println(fact);
+//         }   
+
+//     }
+//2.Написать метод, принимающий на вход два целых числа и проверяющий, что их сумма лежит в пределах от 10 до 20 (включительно),
+// если да – вернуть true, в противном случае – false.
+
+    // public class program{
+    //     public static void main(String[] args) {
+    //         Scanner iScanner = new Scanner(System.in);
+    //         Integer n = iScanner.nextInt();
+    //         Integer m = iScanner.nextInt();
+
+    //         method(n,m);  
+    //     }  
+        
+    //     private static void method(int x, int y) {
+    //         int sum = x+y;
+    //         if (sum<=20 && sum >= 10){
+    //             System.out.println(true);
+    //         }
+    //         else{
+    //             System.out.println(false);
+    //         }   
+    //      }   
+    // }    
